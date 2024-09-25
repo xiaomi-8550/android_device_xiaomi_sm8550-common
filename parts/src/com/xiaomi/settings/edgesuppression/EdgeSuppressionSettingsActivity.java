b262f53a43562fcb584961e24a540e835435fd50
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-25 13:05:53 +0200

    sm8550-common: Replace widget.R with collapsingtoolbar.R within Edge Suppression Service

--- parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionSettingsActivity.java
+++ parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionSettingsActivity.java
@@ -10,7 +10,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class EdgeSuppressionSettingsActivity extends CollapsingToolbarBaseActivity {
 
