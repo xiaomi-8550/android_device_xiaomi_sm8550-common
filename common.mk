6b8fa2a76100a7591a7e900b4b31273279adc560
danielml <daniel@danielml.dev>
2025-01-06 12:11:29 +0100

    sm8550-common: Provide stub 32-bit QESDK libraries for modemManager
    modemManager checks for the existence of these files with a specific
    hash, even if they are not used, so provide a symlink to /dev/null
    and replace the expected hash.
    
    Change-Id: I66ef3166dc3fcd7523d82bd56302bdac7a09bd1d

--- common.mk
+++ common.mk
@@ -237,6 +237,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# ModemManager
+PRODUCT_PACKAGES += \
+    libqesdk_manager_32bit_stub \
+    libqesdk2_0_32bit_stub
+
 # Overlays
 PRODUCT_PACKAGES += \
     Xiaomi8550CarrierConfigRes \
